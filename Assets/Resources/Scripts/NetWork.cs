4042661e4eb03cabe29f54335634c506c4a8dbac
VorbyT <artem.slepentsev@gmail.com>
2017-07-26 05:40:35 +0300

    LAN network manager is set up on test scene + set and get ip scripts

---
+++ Assets/Resources/Scripts/NetWork.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetWork : NetworkManager {
+
+    public static string ip;
+
+	// Use this for initialization
+	void Start () {
+        networkAddress = ip;
+
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Kappa");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        networkAddress = ip;
+    }
+}
---
+++ Assets/setIP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class setIP : MonoBehaviour {
+
+    InputField field;
+
+    void Start()
+    {
+        field = GetComponent<InputField>();
+       // field.text = NetWork.ip;
+    }
+
+	public void btnClick()
+    {
+        NetWork.ip = field.text;
+    }
+}
